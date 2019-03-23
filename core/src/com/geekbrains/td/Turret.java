2e365e45892bcd25778dc0c3653da4eaaba448a4
bobahe <bobahe@gmail.com>
2019-03-23 12:28:34 +0300

    Turret moving implemented

--- core/src/com/geekbrains/td/Turret.java
+++ core/src/com/geekbrains/td/Turret.java
@@ -3,6 +3,7 @@ package com.geekbrains.td;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class Turret {
     private GameScreen gameScreen;
@@ -10,12 +11,14 @@ public class Turret {
     private TextureRegion texture;
     private int cellX, cellY;
     private float angle;
+    private Vector2 direction;
 
     public Turret(GameScreen gameScreen, TextureAtlas atlas) {
         this.gameScreen = gameScreen;
         this.texture = new TextureRegion(atlas.findRegion("turrets"), 0, 0, 80, 80);
         this.cellX = 8;
         this.cellY = 4;
+        direction = new Vector2();
     }
 
     public void render(SpriteBatch batch) {
@@ -23,7 +26,11 @@ public class Turret {
     }
 
     public void update(float dt) {
-        angle += 180.0 * dt;
+        //angle += 180.0 * dt;
+        direction.set(gameScreen.getMonster().getPosition().x - cellX * 80,
+                gameScreen.getMonster().getPosition().y - cellY * 80);
+
+        angle = (float) Math.toDegrees(Math.atan2(direction.y, direction.x));
     }
 
     public void setTurretPosition(int cellX, int cellY) {
